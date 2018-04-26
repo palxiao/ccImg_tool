4bd599d1746c60d6cf1a2718a6d7ee8399b06dd6
Shawn Phang <palxiao@vip.qq.com>
2018-04-26 17:58:39 +0800

    cropper test

--- src/js/main.js
+++ src/js/main.js
@@ -13,33 +13,37 @@ require.config({
   }
 });
 
+var c
+
 require(['cropper'], function (Cropper) {
-  window.addEventListener('DOMContentLoaded', function () {
     var image = document.querySelector('#image');
     var minAspectRatio = 0.5;
     var maxAspectRatio = 1.5;
     var cropper = new Cropper(image, {
       ready: function () {
+        c = this.cropper
         var cropper = this.cropper;
         var containerData = cropper.getContainerData();
         var cropBoxData = cropper.getCropBoxData();
         var aspectRatio = cropBoxData.width / cropBoxData.height;
         var newCropBoxWidth;
-  
+
         if (aspectRatio < minAspectRatio || aspectRatio > maxAspectRatio) {
           newCropBoxWidth = cropBoxData.height * ((minAspectRatio + maxAspectRatio) / 2);
-  
+
           cropper.setCropBoxData({
             left: (containerData.width - newCropBoxWidth) / 2,
             width: newCropBoxWidth
           });
         }
+        cropper.setAspectRatio(1/1)
+        cropper.setDragMode('move')
       },
       cropmove: function () {
         var cropper = this.cropper;
         var cropBoxData = cropper.getCropBoxData();
         var aspectRatio = cropBoxData.width / cropBoxData.height;
-  
+
         if (aspectRatio < minAspectRatio) {
           cropper.setCropBoxData({
             width: cropBoxData.height * minAspectRatio
@@ -49,8 +53,39 @@ require(['cropper'], function (Cropper) {
             width: cropBoxData.height * maxAspectRatio
           });
         }
+      },
+      crop: function (e) {
+        
       }
     });
-  })
-  
 })
+var result = document.querySelector('#result');
+var download = document.getElementById('download');
+function crop() {
+  var image = new Image();
+  // image.setAttribute("crossOrigin", 'Anonymous')
+  image.crossOrigin = 'Anonymous';
+  var can = c.getCroppedCanvas();
+  image.src = can.toDataURL('image/jpeg')
+  // c.getCroppedCanvas().toBlob(function (blob) {
+  //   var formData = new FormData();
+  //   formData.append('croppedImage', blob);
+  //   console.log(formData.get('croppedImage'));
+  // })
+  result.innerHTML=''
+  result.appendChild(image);
+  download.href = c.getCroppedCanvas().toDataURL('image/jpeg')
+}
+
+function selectImg(file) {
+  if (!file.files || !file.files[0]) {
+    return;
+  }
+  
+  var reader = new FileReader();
+  reader.onload = function (evt) {
+    var replaceSrc = evt.target.result;
+    c.replace(replaceSrc);
+  }
+  reader.readAsDataURL(file.files[0]);
+}
