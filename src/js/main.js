b8a704d57d043af1461c5489b361ffe21fb5b753
Shawn Phang <palxiao@vip.qq.com>
2018-04-28 18:02:42 +0800

    .

--- src/js/main.js
+++ src/js/main.js
@@ -16,53 +16,17 @@ require.config({
 
 var c
 var image = document.querySelector('#image');
+var Cropper
 
-require(['cropper', 'toBlob'], function (Cropper) {
+require(['cropper', 'toBlob'], function (myCropper) {
   var minAspectRatio = 0.5;
   var maxAspectRatio = 1.5;
-  var cropper = new Cropper(image, {
-    ready: function () {
-      c = this.cropper
-      var cropper = this.cropper;
-      var containerData = cropper.getContainerData();
-      var cropBoxData = cropper.getCropBoxData();
-      var aspectRatio = cropBoxData.width / cropBoxData.height;
-      var newCropBoxWidth;
-
-      if (aspectRatio < minAspectRatio || aspectRatio > maxAspectRatio) {
-        newCropBoxWidth = cropBoxData.height * ((minAspectRatio + maxAspectRatio) / 2);
-
-        cropper.setCropBoxData({
-          left: (containerData.width - newCropBoxWidth) / 2,
-          width: newCropBoxWidth
-        });
-      }
-      
-      cropper.setAspectRatio('') // 16/9 4/3 2/3 1/1
-      cropper.setDragMode('move')
-    },
-    cropmove: function () {
-      var cropper = this.cropper;
-      var cropBoxData = cropper.getCropBoxData();
-      var aspectRatio = cropBoxData.width / cropBoxData.height;
-
-      if (aspectRatio < minAspectRatio) {
-        cropper.setCropBoxData({
-          width: cropBoxData.height * minAspectRatio
-        });
-      } else if (aspectRatio > maxAspectRatio) {
-        cropper.setCropBoxData({
-          width: cropBoxData.height * maxAspectRatio
-        });
-      }
-    },
-    crop: function (e) {
-
-    }
-  });
+  Cropper = myCropper
 })
 var result = document.querySelector('#result');
 var download = document.getElementById('download');
+var outImg = document.getElementById('outImg')
+var input = document.getElementById('chooseImg');
 function crop() {
   var image = new Image();
   // image.setAttribute("crossOrigin", 'Anonymous')
@@ -73,9 +37,11 @@ function crop() {
     var formData = new FormData();
     formData.append('croppedImage', blob);
   })
-  result.innerHTML=''
-  result.appendChild(image);
+  // result.innerHTML=''
+  // result.appendChild(image);
+  outImg.src = can.toDataURL('image/jpeg');
   download.href = c.getCroppedCanvas().toDataURL('image/jpeg')
+  model('hide')
 }
 
 function selectImg(file) {
@@ -86,7 +52,30 @@ function selectImg(file) {
   var reader = new FileReader();
   reader.onload = function (evt) {
     var replaceSrc = evt.target.result;
-    c.replace(replaceSrc);
+    // c.replace(replaceSrc);
+    input.value = '';
+    image.src = replaceSrc
+    c = new Cropper(image, {
+      aspectRatio: '',
+      ready: function () {
+        // c.setAspectRatio('') // 16/9 4/3 2/3 1/1
+        this.cropper.setDragMode('move')
+      }
+    });
   }
   reader.readAsDataURL(file.files[0]);
+
+  model('show')
+}
+
+function model(type) {
+  if (type === 'show') {
+    // document.getElementById('main').style.display = 'none'
+    document.getElementById('out').style.display = 'table'
+  } else if ('hide') {
+    document.getElementById('main').style.display = ''
+    document.getElementById('out').style.display = 'none'
+    c.destroy();
+    c = null;
+  }
 }
